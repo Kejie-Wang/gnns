523fa789ceda4f8af7bbceef9fa665d5193f9e43
JackWang822 <wang_kejie@foxmail.com>
2017-03-29 13:38:31 +0800

    add the knn graph build and the find neighbor

--- algorithms/knn_graph.h
+++ algorithms/knn_graph.h
@@ -26,14 +26,12 @@ namespace gnns
         NAIVE
     };
 
-    template <typename T, typename Distance>
-    class KNN_Graph
+    template <typename Distance>
+    class Knn_Graph
     {
     public:
         typedef typename Distance::ElementType ElementType;
         typedef typename Distance::DistanceType DistanceType;
-
-        typedef size_t IndexType;
 
     private:
         /*
@@ -50,70 +48,50 @@ namespace gnns
             std::vector<std::pair<DistanceType, IndexType>> v;
             for(int i=0;i<length;++i)
                 v.push_back(std::pair<DistanceType, size_t>(dist[i], i));
-            std::nth_element(v.begin(), v.begin()+n, v.end());
-            std::sort(v.begin(), v.begin()+n);
+
+            //since it contain the point itself and find n+1 minimum elements
+            std::nth_element(v.begin(), v.begin()+n+1, v.end());
+            std::sort(v.begin(), v.begin()+n+1);
+            //eliminate the first element(itself)
             for(size_t i=0;i<n;++i)
             {
-                elements[i] = v[i].first;
-                indices[i] = v[i].second;
+                elements[i] = v[i+1].first;
+                indices[i] = v[i+1].second;
             }
         }
-
 
         /*
          * @brief use a naive construction way to build a knn nearest neighbor graph
          * @param data: the coordinate of the point in matrix type with shape [point_num, dim]
          */
-        void naive_construction(const Matrix<T>& data)
+        void naive_construction(const Matrix<ElementType>& data)
         {
             size_t vec_num = data.rows;
             DistanceType *d = new DistanceType[vec_num*vec_num];
             Matrix<DistanceType> dist(d, vec_num, vec_num);
 
+            //compute the distance between each two points
             for(int i=0;i<data.rows;++i)
             {
                 for(int j=i+1;j<data.rows;++j)
                 {
-                    T* v1 = data[i];
-                    T* v2 = data[j];
-                    dist[i][j] = dist[j][i] = distance_(v1, v2, data.cols);
+                    ElementType* v1 = data[i];
+                    ElementType* v2 = data[j];
+                    dist[i][j] = dist[j][i] = distance(v1, v2, data.cols);
                 }
             }
             for(int i=0;i<data.rows;++i)
             {
-                nth_index_element(dist[i], vec_num, distances[i], indices[i], k_);
+                nth_index_element(dist[i], vec_num, distances[i], indices[i], k);
             }
         }
 
-        void build_graph(const Matrix<T>& data, BUILD_GRAPH_METHOD method)
+        void build_graph(const Matrix<ElementType>& data, BUILD_GRAPH_METHOD method)
         {
             if(method==NAIVE)
             {
                 naive_construction(data);
             }
-        }
-
-
-    public:
-
-        /*Default constructor*/
-        KNN_Graph(){}
-
-        /*
-         *
-         * */
-        KNN_Graph(Matrix<T> data, const size_t k = 1000, BUILD_GRAPH_METHOD method=NAIVE, bool rebuild = false)
-        {
-            k_ = k;
-            indices = Matrix<IndexType>(new IndexType[data.rows*k], data.rows, k);
-            distances = Matrix<DistanceType>(new DistanceType[data.rows*k], data.rows, k);
-            build_graph(data, method);
-        }
-
-        ~KNN_Graph()
-        {
-            delete[] indices.ptr();
-            delete[] distances.ptr();
         }
 
         /*
@@ -122,14 +100,46 @@ namespace gnns
          * @param dist_file_path: the file path of the distance file
          * @exception define in the io.h
          */
-        void load_graph(const std::string& index_file_path, const std::string& dist_file_path)
+        void load_graph(const std::string& graph_index_path, const std::string& graph_dist_path)
         {
             try{
-                indices = load_from_file<IndexType>(index_file_path);
-                distances = load_from_file<DistanceType>(dist_file_path);
+                indices = load_from_file<IndexType>(graph_index_path);
+                distances = load_from_file<DistanceType>(graph_dist_path);
             }catch (std::exception& e){
                 throw e;
             }
+        }
+
+    public:
+
+        /*Default constructor*/
+        Knn_Graph(){}
+
+        /*
+         *
+         * */
+        Knn_Graph(const Matrix<ElementType>& data, const size_t k = 1000, BUILD_GRAPH_METHOD method=NAIVE)
+        {
+            this->k = k;
+            indices = Matrix<IndexType>(new IndexType[data.rows*k], data.rows, k);
+            distances = Matrix<DistanceType>(new DistanceType[data.rows*k], data.rows, k);
+            build_graph(data, method);
+        }
+
+        /*
+         *
+         * */
+        Knn_Graph(const std::string& graph_index_path, const std::string& graph_dist_path)
+        {
+            load_graph(graph_index_path, graph_dist_path);
+            k = indices.cols;
+        }
+
+
+        ~Knn_Graph()
+        {
+            delete[] indices.ptr();
+            delete[] distances.ptr();
         }
 
         /*
@@ -148,14 +158,54 @@ namespace gnns
             }
         }
 
+        void get_neighbors(const IndexType search_index,
+            std::vector<IndexType> indices,
+            std::vector<DistanceType> dists,
+             int graph_search_expand=-1)
+        {
+            if(graph_search_expand == -1)
+            {
+                graph_search_expand = k;
+            }
+
+            indices.resize(graph_search_expand);
+            dists.resize(graph_search_expand);
+            for(int i=0;i<graph_search_expand;++i)
+            {
+                indices[i] = this->indices[search_index][i];
+                dists[i] = this->distances[search_index][i];
+            }
+        }
+
+        std::vector<IndexType> get_neighbors(const IndexType search_index, int graph_search_expand=-1)
+        {
+            if(graph_search_expand == -1)
+            {
+                graph_search_expand = k;
+            }
+
+            std::vector<IndexType> neighbors;
+            neighbors.resize(graph_search_expand);
+            for(int i=0;i<graph_search_expand;++i)
+            {
+                neighbors[i] = this->indices[search_index][i];
+            }
+
+            return neighbors;
+        }
+
     private:
+
+        //distance of the two points
         Matrix<DistanceType> distances;
+
+        //nearst neighbor index
         Matrix<IndexType> indices;
 
-        size_t k_;
+        //k nearst neighbor in the graph
+        size_t k;
 
-        Distance distance_;
-
+        Distance distance;
     };
 
 }
